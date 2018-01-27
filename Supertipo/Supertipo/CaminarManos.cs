6fa18c817f69fccf18279fe060ef140362d4ab47
Isaac sosa <ss17gm@gmail.com>
2018-01-26 19:38:37 -0800

    Initial Commit

---
+++ Supertipo/Supertipo/CaminarManos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supertipo
+{
+    class CaminarManos : IChanginBehavior
+    {
+        public void Caminar()
+        {
+            Console.WriteLine("Yo camino Con Manos");
+        }
+    }
+}
