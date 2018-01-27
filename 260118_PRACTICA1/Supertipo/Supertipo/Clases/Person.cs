a11d1de4818cf36d52a46157ee041eb03512d062
Isaac sosa <ss17gm@gmail.com>
2018-01-26 19:48:05 -0800

    +Adding Frist Activity CLass

---
+++ 260118_PRACTICA1/Supertipo/Supertipo/Clases/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supertipo
+{
+    class Person
+    {
+        public string Name;
+        public int Age;
+        public  IChanginBehavior WalkingBehavior;
+
+
+      
+        public void Comer()
+        {
+            Console.WriteLine("Yo Como");
+
+        }
+
+        public  void Defecar()
+        {
+            Console.WriteLine("Yo Defeco");
+
+        }
+
+        public void Caminar()
+        {
+            WalkingBehavior.Caminar();
+
+        }
+    }
+}
