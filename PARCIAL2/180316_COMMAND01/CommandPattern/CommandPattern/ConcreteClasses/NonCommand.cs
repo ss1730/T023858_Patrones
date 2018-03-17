f725713cb71ef250cf3265aacb09b99ec0713b3e
Isaac sosa <ss17gm@gmail.com>
2018-03-16 21:22:24 -0800

    +adding list<command>

---
+++ PARCIAL2/180316_COMMAND01/CommandPattern/CommandPattern/ConcreteClasses/NonCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandPattern.Interfaces;
+
+namespace CommandPattern.ConcreteClasses
+{
+    class NonCommand:ICommand
+    {
+        public void Execute()
+        {
+           
+        }
+
+        public void Undo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Recive()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
